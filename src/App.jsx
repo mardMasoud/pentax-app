6f3cb18406f30a9924c772d8e1608e9f3389d2cc
masoudMarde <mardmasoud66@gmail.com>
2023-06-19 00:23:36 +0330

    v_0

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Header from './components/header/Header'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Header/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header ">
+      <div className="container">
+        <div className="header__left">
+            
+        </div>
+        <div className="header__right"></div>
+      </div>
+
+     
+    </div>
+  );
+}
+
+export default Header;
