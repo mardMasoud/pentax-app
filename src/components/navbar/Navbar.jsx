1386932be8506b6cfaaecec162da2578f9800db7
masoudMarde <mardmasoud66@gmail.com>
2023-07-07 15:19:47 +0330

    v3

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,33 +1,42 @@
 import React, { useState } from "react";
 import "./Navbar.css";
+import Collapse from "react-bootstrap/Collapse";
+import Hamburger from 'hamburger-react'
+
 function Navbar() {
   const listMenu = [" HOME", "ABOUT", "SERVICES", "PROJECT", "BLOG", "CONTACT"];
-  const [show, setshow] = useState("none");
-  function show1() {
-    if (show == "none") setshow("block");
-    else setshow("none");
+ 
+   
+  const [open, setOpen1] = useState(false);
+  const [isOpen, setOpen] = useState(false)
+  function togglemenu() {
+    setOpen1(!open);
+
+   
   }
   return (
     <div className="nav">
       <div className="container">
-        <div className="row align-items-center py-4">
+        <div className="row align-items-center py-4 position-relative">
           <div className="logo col-6 ">
             <img src="images/logo1.webp" alt="" className="logo__img" />
           </div>
-
-          <div className="hamber col-6 d-lg-none align-items-end " onClick={show1}>
-            <div className="child child1"></div>
-            <div className="child child2"></div>
-            <div className="child child3"></div>
+          <div className="hamber1 col-6 d-lg-none justify-content-center" onClick={togglemenu}> 
+          <Hamburger toggled={isOpen} toggle={setOpen}    aria-controls="menu"
+            aria-expanded={open}/>
           </div>
-          <div className="menu col-12 pt-3 " style={{ display: show }}>
-            <ul>
-              {listMenu.map((item) => (
-      
-                <a href="#" className="text-dark"><li className="border p-4">{item}</li></a>
-              ))}
-            </ul>
-          </div>
+       
+          <Collapse in={open} timeout={500}>
+            <div className=" col-12 pt-3" id="menu">
+              <ul>
+                {listMenu.map((item) => (
+                  <a href="#" key={item} className="text-dark">
+                    <li className="border p-4">{item}</li>
+                  </a>
+                ))}
+              </ul>
+            </div>
+          </Collapse>
         </div>
       </div>
     </div>
