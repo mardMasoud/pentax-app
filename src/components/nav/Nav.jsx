0415b7e1d76d224a253e3c1ea14228f1d0414815
masoudMarde <mardmasoud66@gmail.com>
2023-07-17 22:06:12 +0330

    v7

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,68 +1,70 @@
 import React, { useState } from "react";
 import "./Nav.css";
 import Collapse from "react-bootstrap/Collapse";
-import Hamburger from "hamburger-react";
+import { Sling as Hamburger } from "hamburger-react";
 import { FaSearch } from "react-icons/fa";
 
 function Nav() {
   const listMenu = [" HOME", "ABOUT", "SERVICES", "PROJECT", "BLOG", "CONTACT"];
 
   const [isOpen, setOpen] = useState(false);
-  const [Open,setopen]=useState(false)
- window.addEventListener('resize',handlshow)
- function handlshow(){
-    
-    if(window.innerWidth>=992)
-     setopen(true)
-     if(window.innerWidth<=991){
-      setopen(false)
-      setOpen(false)
-     }
-    
+  const [isActive, setopen] = useState(false);
+  window.addEventListener("resize", handlshow);
+  function handlshow() {
+    if (window.innerWidth >= 992) setopen(true);
+    if (window.innerWidth <= 991) {
+      setopen(false);
+      setOpen(false);
+    }
   }
   return (
-    
-      
-    <div className="nav1 fixed-top">
+    <div className="nav1">
       <div className="container">
-        <div className="row align-items-center">
-          <div className="logo col-6 col-lg-3 mt-4">
+        <div className="row  align-items-center ">
+          <div className="logo col-11  col-lg-4  ">
             <img src="images/logo1.webp" alt="" className="logo__img" />
           </div>
-          <div className="col-6 d-lg-none d-flex justify-content-end mt-4" >
-            <div className="ber" onClick={()=>setopen(!Open)} > <Hamburger 
-              toggled={isOpen}
-              toggle={setOpen}
-              
-              color="#f81c1c"
-              size="25"
-              aria-controls="menu"
-              aria-expanded={isOpen}
-            /></div>
-           
-          </div>
-
-          <Collapse in={Open} timeout={100}>
-            <div className=" col-12 col-lg-9 pt-3  " id="menu">
-              <ul className="d-lg-flex">
-                {listMenu.map((item) => (
-                  <a href="#" key={item} className="text-dark">
-                    <li className="py-3 px-lg-4">{item}</li>
-                  </a>
-                ))}
-                <a href="#" className="text-dark ">
-                  <li className=" py-4 py-lg-3 ">
-                    <FaSearch />
-                  </li>
-                </a>
-              </ul>
+          <div className="col-1 d-lg-none py-3   ">
+            <div className="ber ">
+              <Hamburger
+                toggled={isOpen}
+                toggle={setOpen}
+                onToggle={(toggled) => {
+                  setopen(!isActive);
+                }}
+                color="#f81c1c"
+                size="25"
+                aria-controls="menu"
+                aria-expanded={isActive}
+              />
             </div>
+          </div>
+          <div className=" col-12 col-lg-8 ">
+          <Collapse in={isActive}>
+            <ul className="d-lg-flex  " id="menu">
+              {listMenu.map((item, index) => (
+                // <a href="#" key={item} className="text-dark">
+                //   <li className="py-3 px-lg-4">{item}</li>
+                // </a>
+                <li className="pb-4 px-lg-4  py-lg-3" key={index}>
+                  <a href="#" className="text-dark">
+                    {item}
+                  </a>
+                </li>
+              ))}
+              <li className="pb-4 py-lg-3 ">
+                <a href="#" className="text-dark">
+                  <FaSearch />
+                </a>
+              </li>
+            </ul>
           </Collapse>
         </div>
+        </div>
+
+       
       </div>
     </div>
-   
-   
   );
 }
 
